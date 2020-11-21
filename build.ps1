9849320c6f8fcfa601157733e5eef07476ac30e1
Seukwon Kang <kasworld@gmail.com>
2020-11-21 14:15:34 +0900

    add cross compile

--- build.ps1
+++ build.ps1
@@ -36,3 +36,27 @@ Write-Output "package dataversion
 const DataVersion = `"${Data_VERSION}`" 
 " > config/dataversion/dataversion_gen.go 
 
+################################################################################
+$DATESTR=Get-Date -UFormat '+%Y-%m-%dT%H:%M:%S%Z:00'
+$GITSTR=git rev-parse HEAD
+################################################################################
+# build bin
+
+$BIN_DIR="bin"
+$SRC_DIR="."
+
+mkdir -ErrorAction SilentlyContinue "${BIN_DIR}"
+
+$BUILD_VER="${DATESTR}_${GITSTR}_release_windows"
+Write-Output "Build Version: ${BUILD_VER}"
+Write-Output ${BUILD_VER} > ${BIN_DIR}/BUILD_windows
+go build -o "${BIN_DIR}\server.exe" -ldflags "-X main.Ver=${BUILD_VER}" "${SRC_DIR}\server.go"
+go build -o "${BIN_DIR}\multiclient.exe" -ldflags "-X main.Ver=${BUILD_VER}" "${SRC_DIR}\multiclient.go"
+
+$BUILD_VER="${DATESTR}_${GITSTR}_release_linux"
+Write-Output "Build Version: ${BUILD_VER}"
+Write-Output ${BUILD_VER} > ${BIN_DIR}/BUILD_linux
+$env:GOOS="linux" 
+go build -o "${BIN_DIR}\server" -ldflags "-X main.Ver=${BUILD_VER}" "${SRC_DIR}\server.go"
+go build -o "${BIN_DIR}\multiclient" -ldflags "-X main.Ver=${BUILD_VER}" "${SRC_DIR}\multiclient.go"
+$env:GOOS=""
