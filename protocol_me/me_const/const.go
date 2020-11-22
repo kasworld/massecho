13321367170548e5c89591f112443665a9ee49a2
Seukwon Kang <kasworld@gmail.com>
2020-11-22 16:04:04 +0900

    adjust me_const

--- protocol_me/me_const/const.go
+++ protocol_me/me_const/const.go
@@ -6,9 +6,9 @@ import "time"
 
 const (
 	// MaxBodyLen set to max body len, affect send/recv buffer size
-	MaxBodyLen = 0xfffff
+	MaxBodyLen = 0xffff
 	// PacketBufferPoolSize max size of pool packet buffer
-	PacketBufferPoolSize = 100
+	PacketBufferPoolSize = 10
 
 	// ServerAPICallTimeOutDur api call watchdog timer
 	ServerAPICallTimeOutDur = time.Second * 2
